5def21496566d12c9a72067c5d9894ff654b3325
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-20 15:53:25 +0700

    errors test

--- utils/errorHandle.js
+++ utils/errorHandle.js
@@ -5,8 +5,10 @@ const DEFAULT_ERROR = 500;
 module.exports.errorHandle = (err, res) => {
   if (err.name === "ValidationError") {
     res.status(VALIDATION_ERROR).send({ message: 'Некорректные данные' })
-  } else if (err.name === 'CastError' || err.message === 'NotFound') {
+  } else if (err.message === 'NotFound') {
     res.status(NOT_FOUND_ERROR).send({ message: 'Данные не найдены' })
+  } else if (err.name === 'CastError') {
+    res.status(VALIDATION_ERROR).send({ message: 'Некорректные данные' })
   }
   else {
     res.status(DEFAULT_ERROR).send({ message: 'Произошла ошибка' })
