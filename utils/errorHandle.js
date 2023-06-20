4b526711801cae469a0e737858f1e1c21279596d
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-20 15:36:49 +0700

    errors test

--- utils/errorHandle.js
+++ utils/errorHandle.js
@@ -5,10 +5,8 @@ const DEFAULT_ERROR = 500;
 module.exports.errorHandle = (err, res) => {
   if (err.name === "ValidationError") {
     res.status(VALIDATION_ERROR).send({ message: 'Некорректные данные' })
-  } else if (err.name === 'CastError' || err.message === 'NotFoundUser') {
+  } else if (err.name === 'CastError' || err.message === 'NotFound') {
     res.status(NOT_FOUND_ERROR).send({ message: 'Данные не найдены' })
-  } else if (err.message === 'NotFoundCard') {
-    res.status(VALIDATION_ERROR).send({ message: 'Данные не найдены' })
   }
   else {
     res.status(DEFAULT_ERROR).send({ message: 'Произошла ошибка' })
