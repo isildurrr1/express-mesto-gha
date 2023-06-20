d29164326d9c8feba61a11db71b310d74b5991ba
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-20 13:41:34 +0700

    users errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,11 @@ const Card = require('../models/card');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then(cards => res.send({ data: cards }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() =>
+    {
+      if (res.status(400)) res.send({ message: 'Произошла ошибка 400' })
+      else res.send({ message: 'Произошла ошибка' })
+    });
 };
 
 module.exports.createCard = (req, res) => {
@@ -13,20 +17,33 @@ module.exports.createCard = (req, res) => {
   const createdAt = Date().now;
   Card.create({ name, link, owner, likes, createdAt })
     .then(card => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() =>
+    {
+      if (res.status(400)) res.send({ message: 'Произошла ошибка 400' })
+      else res.send({ message: 'Произошла ошибка' })
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.deleteOne({ _id: req.params.cardId })
     .then(() => res.send({ message: 'Пост удалён' }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() =>
+    {
+      if (res.status(404)) res.send({ message: 'Произошла ошибка 404' })
+      else res.send({ message: 'Произошла ошибка' })
+    });
 }
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate( req.params.cardId,
     { $addToSet: { likes: req.user._id } },{ new: true })
     .then(card => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() =>
+    {
+      if (res.status(404)) res.send({ message: 'Произошла ошибка 404' })
+      else if (res.status(400)) res.send({ message: 'Произошла ошибка 400' })
+      else res.send({ message: 'Произошла ошибка' })
+    });
 }
 
 
@@ -34,5 +51,10 @@ module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId,
     { $pull: { likes: req.user._id } }, { new: true })
     .then(card => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() =>
+    {
+      if (res.status(404)) res.send({ message: 'Произошла ошибка 404' })
+      else if (res.status(400)) res.send({ message: 'Произошла ошибка 400' })
+      else res.send({ message: 'Произошла ошибка' })
+    });
 }
