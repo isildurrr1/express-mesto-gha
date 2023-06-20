0920e6c41347de83813a7336d34313262d5da68b
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-20 15:47:49 +0700

    errors test

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use('/users', require('./routes/users'));
 
 app.use('/cards', require('./routes/cards'));
 
-app.use((req, res) => res.status(500).send({ message: 'Введите корректный путь' }));
+app.use((req, res) => res.status(404).send({ message: 'Введите корректный путь' }));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
