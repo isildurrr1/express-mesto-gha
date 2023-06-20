7f17c12ebc4e58e087cde54f3eb83572506fc786
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-20 12:35:01 +0700

    add all functionality without errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,13 +11,28 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   const likes = [];
   const createdAt = Date().now;
-  Card.create({ name, link, owner, likes, createdAt})
+  Card.create({ name, link, owner, likes, createdAt })
     .then(card => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.deleteOne({_id: req.params.cardId})
-  .then(() => res.send({ message: 'Пост удалён'} ))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  Card.deleteOne({ _id: req.params.cardId })
+    .then(() => res.send({ message: 'Пост удалён' }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate( req.params.cardId,
+    { $addToSet: { likes: req.user._id } },{ new: true })
+    .then(card => res.send(card))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId,
+    { $pull: { likes: req.user._id } }, { new: true })
+    .then(card => res.send(card))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 const Card = require('../models/card');
-const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard
+} = require('../controllers/cards');
 
 router.get('/', getCards); // GET /cards — возвращает все карточки
 
 router.post('/', createCard); // POST /cards — создаёт карточку
 
-router.delete('/:cardId', deleteCard); // DELETE /cards/:cardId — удаляет карточку по идентификатору 
+router.delete('/:cardId', deleteCard); // DELETE /cards/:cardId — удаляет карточку по идентификатору
+
+router.put('/:cardId/likes', likeCard); // PUT /cards/:cardId/likes — поставить лайк карточке
+
+router.delete('/:cardId/likes', dislikeCard); // DELETE /cards/:cardId/likes — убрать лайк с карточки
 
 module.exports = router;
