0e6152f009348e8dad957257a2e1cf0c0b4de7e0
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-21 15:32:20 +0700

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -21,13 +21,17 @@ module.exports.createUser = (req, res) => {
     .catch((err) => errorHandle(err, res));
 };
 
-const findAndUpdate = (req, res) => {
-  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail(() => { throw new Error('NotFound'); })
     .then((user) => res.send(user))
     .catch((err) => errorHandle(err, res));
 };
 
-module.exports.updateProfile = (req, res) => findAndUpdate(req, res);
-
-module.exports.updateAvatar = (req, res) => findAndUpdate(req, res);
+module.exports.updateAvatar = (req, res) => {
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+    .orFail(() => { throw new Error('NotFound'); })
+    .then((user) => res.send(user))
+    .catch((err) => errorHandle(err, res));
+};
